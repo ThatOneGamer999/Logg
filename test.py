30aad9ee3a15a9ef5a62c34d89ae884b2426e065
ThatOneGamer999 <keenonsalish@icloud.com>
2016-12-11 19:22:48 -0400

    Update test.py

--- test.py
+++ test.py
@@ -8,6 +8,11 @@ else:
  print "Logging Creation \t\t\t\t\t[OK]"
 log('Info')
 print "Logging          \t\t\t\t\t[OK]"
+f = open('log.txt', 'r');
+l = f.readlines()
+for i in l:
+ print i
+f.close()
 remlog()
 if os.path.isfile('log.txt'): raise Exception('error!')
 else: print "Logging Deletion \t\t\t\t\t[OK]"
