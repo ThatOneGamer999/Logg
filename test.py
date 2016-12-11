5664b2198bf95bf72929cf5161408a843d3c6b8f
ThatOneGamer999 <keenonsalish@icloud.com>
2016-12-11 14:48:10 -0800

    Update test.py

--- test.py
+++ test.py
@@ -4,9 +4,9 @@ makelog()
 if not os.path.isfile('log.txt'):
  raise Exception('error!')
 else:
- print "Logging Creation \t\t\t\t\t[OK]
+ print "Logging Creation \t\t\t\t\t[OK]"
 log('Info')
 print "Logging          \t\t\t\t\t[OK]"
 remlog()
 if os.path.isfile('log.txt'): raise Exception('error!')
-else: print "Logging Deletion \t\t\t\t\t[OK]
+else: print "Logging Deletion \t\t\t\t\t[OK]"
