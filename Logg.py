2017379451c3f4b8b5e9555bdf609e2c4495ba0a
ThatOneGamer999 <keenonsalish@icloud.com>
2016-12-11 14:08:22 -0800

    Add files via upload

---
+++ Logg.py
@@ -0,0 +1,26 @@
+import sys
+import os
+import datetime
+def makelog(name='log.txt'):
+    log = open(str(name), 'w')
+    log.close()
+def log(msg, lvl='info', logfile='log.txt'):
+    if not os.path.isfile(logfile):
+        makelog()
+    t = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    f = open(logfile, 'a')
+    if lvl == 'info':
+        string = '[' + str(t) + '] [INFO]: ' + str(msg) + '\n'
+    elif lvl == 'warn':
+        string = '[' + str(t) + '] [WARN]: ' + str(msg) + '\n'
+    elif lvl == 'error':
+        string = '[' + str(t) + '] [ERROR]: ' + str(msg) + '\n'
+    else:
+        raise Exception('Invalid String!')
+    f.write(string)
+    f.close()
+def remlog(log='log.txt'):
+    os.remove(log)
+    
+        
+    
